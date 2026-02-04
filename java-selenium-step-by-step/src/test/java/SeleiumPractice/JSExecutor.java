35237ada5db2925755715bac5e2cf8a3b0d1e213
priyanka rahul pujari <pujari.priyanka1010@gmail.com>
2026-02-04 18:48:43 +0530

    added some selenium practises like js, screenshot, tabs, windows, etc

---
+++ java-selenium-step-by-step/src/test/java/SeleiumPractice/JSExecutor.java
@@ -0,0 +1,66 @@
+package SeleiumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JSExecutor {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+		
+		driver.get("https://www.youtube.com/");
+		
+		//js for sendkeys
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('value','priyanka')", driver.findElement(By.name("search_query")));
+		Thread.sleep(2000);
+		
+		String parentID = driver.getWindowHandle();
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://www.worldometers.info/geography/flags-of-the-world/");
+		
+		js.executeScript("arguments[0].click()", driver.findElement(By.xpath("//*[@alt='Australia']")));
+		Thread.sleep(2000);
+		driver.switchTo().window(parentID);
+		
+		driver.switchTo().newWindow(WindowType.WINDOW);
+		driver.get("https://www.worldometers.info/geography/flags-of-the-world/");
+		
+		js.executeScript("arguments[0].scrollIntoView()", driver.findElement(By.xpath("//*[@alt='India']")));
+		
+		
+		Thread.sleep(2000);
+		
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+		Thread.sleep(2000);
+		
+		js.executeScript("window.scrollBy(0,-document.body.scrollHeight)");
+		Thread.sleep(2000);
+		
+		//file upload using JSExecutor
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://davidwalsh.name/demo/multiple-file-upload.php");
+		driver.findElement(By.id("filesToUpload")).sendKeys("E:\\automation\\Senior-software-engineer\\selenium4\\java-selenium-step-by-step\\Files\\Name.txt");
+		String fileName = driver.findElement(By.cssSelector("#fileList >li")).getText();
+		if(fileName.equals("Name.txt")) {
+			System.out.println("File uploaded successfully");
+		}else {
+			System.out.println("Failed to upload the file");
+		}
+		
+		
+		driver.quit();
+	}
+
+}
